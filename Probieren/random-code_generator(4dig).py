9debcbe843d6bd51ce80e35b05e6ba316d23ae14
MaxMohr <mmohr@brg-pichelmayergasse.at>
2023-01-13 23:16:04 +0100

    Probieren hinzugefügt

---
+++ Probieren/random-code_generator(4dig).py
@@ -0,0 +1,9 @@
+import random
+x = 1
+while True:
+    n1 = random.randint(0, 9)
+    n2 = random.randint(0, 9)
+    n3 = random.randint(0, 9)
+    n4 = random.randint(0, 9)
+    print(f"Randomcode: {n1}{n2}{n3}{n4}")
+x += 1

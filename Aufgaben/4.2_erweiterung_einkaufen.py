ee5c3ce2da29a34bb2fe091412ddb9f4f4758b05
MaxMohr <mmohr@brg-pichelmayergasse.at>
2023-01-19 11:32:27 +0100

    Assignment 4.2.: fixed

--- Aufgaben/4.2_erweiterung_einkaufen.py
+++ Aufgaben/4.2_erweiterung_einkaufen.py
@@ -4,8 +4,5 @@ einkaufsliste_anzahl = [2, 1, 5, 1, 100, 10]
 einkaufsliste = zip(einkaufsliste_anzahl, einkaufsliste_gegenstaende)
 
 
-for einkaufsliste_anzahl, einkaufsliste_gegenstaende in zip:
-    print(f'Ich soll heute {einkaufsliste_anzahl}x {einkaufsliste_gegenstaende} einkaufen gehen.')
-
-#for anzahl in einkaufsliste:
-#    print(f'Ich soll heute {anzahl}x einkaufen gehen.')
+for anzahl, gegenstand in einkaufsliste:
+    print(f'Ich soll heute {anzahl}x {gegenstand} einkaufen gehen.')
